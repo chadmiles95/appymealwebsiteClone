d70f4dfd63bb398581741a45d5ec997bbb94f72a
Zack Anselm <rili.main@gmail.com>
2023-06-25 09:37:38 -0500

    fix: favicon and head metadata

--- pages/restaurants/[restaurantname].tsx
+++ pages/restaurants/[restaurantname].tsx
@@ -4,6 +4,7 @@ import { RestaurantPage } from "../../components/RestaurantPage";
 import useFetchRestaurantByName from "../../redux/useFetchRestaurantByName";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../components/Loading";
+import Head from "next/head";
 
 const RestaurantDetailsPage = () => {
   const router = useRouter();
@@ -14,7 +15,17 @@ const RestaurantDetailsPage = () => {
   const restaurants = useSelector((state: any) => state.shopper.restaurants);
   const restaurant = restaurants.find((r: any) => r.name === restaurantname);
 
-  return restaurant ? <RestaurantPage restaurant={restaurant} /> : <Loading />;
+  return (
+    <>
+      <Head>
+        <title>Appymeal.com | {restaurantname}</title>
+        <meta name="description" content="Restaurant delivery & pickup service" />
+      </Head>
+      {
+        restaurant ? <RestaurantPage restaurant={restaurant} /> : <Loading />
+      }
+    </>
+  );
 };
 
 export default RestaurantDetailsPage;
