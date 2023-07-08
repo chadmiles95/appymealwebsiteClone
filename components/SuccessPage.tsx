2df7108a127c9d1b9414e3356f0b1255bcc1462f
Blue Raccoon <zachary_owen@aol.com>
2023-07-07 20:27:16 -0500

    fixing formatting for email and testing.

--- components/SuccessPage.tsx
+++ components/SuccessPage.tsx
@@ -13,7 +13,7 @@ const SuccessPage = () => {
 
   return (
     <div className="flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-2xl text-primary font-semibold">
+      <h1 className="text-2xl text-primary font-semibold pl-12 pr-12 md:pr-0 md:pl-0">
         Thank you for helping us support local restaurants!
       </h1>
 
@@ -22,22 +22,21 @@ const SuccessPage = () => {
       <div></div>
 
       {/* change link later to tracking page for order and remove reset cart? or keep it and take to tracking page for specific order I guess. Could just provide them url and even from app?*/}
-      <Link href="/">
-        <button
-          onClick={() => dispatch(resetCart())}
-          className="text-lg text-lightText hover:underline underline-offset-4 decoration-[1px] hover:text-blue duration-300"
-        >
-          <h2 className="text-dark">Order Details:</h2>
-          <p className="text-dark">Order Number: {order.number}</p>
-          <p className="text-dark">
-            Expected pickup time: {order.expectedPickupTime}
-          </p>
-          <p className="text-dark">Restaurant: {order.restaurant}</p>
-          <p className="text-dark">
-            Address: {order.doorDashInfo.pickup_address}
-          </p>
-        </button>
-      </Link>
+
+      <button
+        // onClick={() => dispatch(resetCart())}
+        className="text-lg text-dark "
+      >
+        <h2 className="text-dark">Order Details:</h2>
+        <p className="text-dark">Order Number: {order?.number}</p>
+        <p className="text-dark">
+          Expected pickup time: {order?.expectedPickupTime}
+        </p>
+        <p className="text-dark">Restaurant: {order?.restaurant}</p>
+        <p className="text-dark">
+          Address: {order?.doorDashInfo?.pickup_address}
+        </p>
+      </button>
     </div>
   );
 };
