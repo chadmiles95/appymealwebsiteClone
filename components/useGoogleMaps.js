d6205f0ef293c4e32c8cb0f133f6cf6d7c1cbb1c
Zack Anselm <rili.main@gmail.com>
2023-09-07 11:06:07 -0500

    fix: hacky autocomplete fix

--- components/useGoogleMaps.js
+++ components/useGoogleMaps.js
@@ -1,27 +1,30 @@
 import { useEffect } from "react";
 
-const isMyScriptLoaded = (url) => {
-  var scripts = document.getElementsByTagName('script');
-  for (var i = scripts.length; i--;) {
-      if (scripts[i].src == url) return true;
-  }
-  return false;
+const SCRIPT_ID = 'appymeal-maps-api-script';
+
+const getScriptById = (scriptId) => {
+  const script = document.getElementById(scriptId);
+  return script;
 }
 
 const useGoogleMaps = (callback) => {
   useEffect(() => {
     const scriptUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places&callback=initMap`;
+    const existingScript = getScriptById(SCRIPT_ID)
+    if (existingScript) {
+      // Remove so we don't accidentally load it multiple times
+      existingScript.remove();
+    }
 
-    if (!isMyScriptLoaded(scriptUrl)) {
-      const noop = () => {};
-      window.initMap = callback || noop;
+    const noop = () => {};
+      window.initMap = callback;
       const script = document.createElement("script");
       script.async = true;
       script.defer = true;
       script.type = "text/javascript";
       script.src = scriptUrl;
+      script.id = SCRIPT_ID;
       document.head.appendChild(script);
-    }
     
   }, [callback]);
 };
